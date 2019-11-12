a1b5930cc0641541f8cc2ee89ac8ec56057ebeb8
Ayaz Khan <khn.ayzahmd@gmail.com>
2019-11-12 03:22:35 -0800

    UPDATE: Configured mongoose, defined models

--- server/repository/mongo/index.js
+++ server/repository/mongo/index.js
@@ -1,15 +1,22 @@
 const mongoose = require('mongoose');
 const TweetSchema = require('./models/tweets');
-const FeedSchema = require('./models/feeds');
 const ReplySchema = require('./models/replies');
 const ConversationSchema = require('./models/conversation');
 
 mongoose.connect(process.env.MONGO_URL);
 
 const tweetModel = mongoose.model('tweets', TweetSchema);
-const feedModel = mongoose.model('feeds', FeedSchema);
 const replyModel = mongoose.model('replies', ReplySchema);
 const conversationModel = mongoose.model('conversation', ConversationSchema);
+
+/*
+TODO
+- tweets by HashTag
+- tweets by User
+- tweets by List
+- tweets by Subscriptions
+- Bookmarked Tweets
+*/
 
 module.exports = {
     Tweet: tweetModel,
