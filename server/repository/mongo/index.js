70076100aa11bd546409818d20b1508904854544
vijay2502 <vijay.ghanshani@sjsu.edu>
2019-11-12 05:51:32 -0800

    added tweet-by-hashtag model

--- server/repository/mongo/index.js
+++ server/repository/mongo/index.js
@@ -2,18 +2,19 @@ const mongoose = require('mongoose');
 const TweetSchema = require('./models/tweets');
 const ReplySchema = require('./models/replies');
 const ConversationSchema = require('./models/conversation');
+const TweetByHashtagSchema = require('./models/tweets-by-hashtag');
 
 mongoose.connect(process.env.MONGO_URL);
 
 const tweetModel = mongoose.model('tweets', TweetSchema);
 const replyModel = mongoose.model('replies', ReplySchema);
 const conversationModel = mongoose.model('conversation', ConversationSchema);
-
+const tweetByHashtagModel = mongoose.model('tweets-by-hashtag', TweetByHashtagSchema);
 /*
 TODO
-- tweets by HashTag
+- tweets by HashTag  --- done
 - tweets by User
-- tweets by List
+- tweets by List  
 - tweets by Subscriptions
 - Bookmarked Tweets
 */
@@ -23,5 +24,6 @@ module.exports = {
     Feed: feedModel,
     Reply: replyModel,
     Conversation: conversationModel,
+    TweetByHashtagModel: tweetByHashtagModel,
     mongoose: mongoose
 };
---
+++ server/repository/mongo/models/tweets-by-hashtag/index.js
@@ -0,0 +1,11 @@
+const Schema = require('mongoose').Schema;
+const tweetSchema = require('../tweets');
+
+const TweetByHashtagSchema = new Schema({
+    hashTags: String,
+    tweets: [tweetSchema]
+}, {
+    timestamps: true
+});
+
+module.exports = TweetByHashtagSchema;
