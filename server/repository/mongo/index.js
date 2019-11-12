5d0045778273885b796a19257c91a9812a637890
vijay2502 <vijay.ghanshani@sjsu.edu>
2019-11-12 06:05:07 -0800

    added tweets-by-list model

--- server/repository/mongo/index.js
+++ server/repository/mongo/index.js
@@ -2,19 +2,21 @@ const mongoose = require('mongoose');
 const TweetSchema = require('./models/tweets');
 const ReplySchema = require('./models/replies');
 const ConversationSchema = require('./models/conversation');
-const TweetByHashtagSchema = require('./models/tweets-by-hashtag');
+const TweetsByHashtagSchema = require('./models/tweets-by-hashtag');
+const TweetsByListSchema = require('./models/tweets-by-list');
 
 mongoose.connect(process.env.MONGO_URL);
 
 const tweetModel = mongoose.model('tweets', TweetSchema);
 const replyModel = mongoose.model('replies', ReplySchema);
 const conversationModel = mongoose.model('conversation', ConversationSchema);
-const tweetByHashtagModel = mongoose.model('tweets-by-hashtag', TweetByHashtagSchema);
+const tweetsByHashtagModel = mongoose.model('tweets-by-hashtag', TweetsByHashtagSchema);
+const tweetsByListModel = mongoose.model('tweets-by-hashtag', TweetsByListSchema);
 /*
 TODO
 - tweets by HashTag  --- done
 - tweets by User
-- tweets by List  
+- tweets by List  --- done
 - tweets by Subscriptions
 - Bookmarked Tweets
 */
@@ -24,6 +26,7 @@ module.exports = {
     Feed: feedModel,
     Reply: replyModel,
     Conversation: conversationModel,
-    TweetByHashtagModel: tweetByHashtagModel,
+    TweetsByHashtagModel: tweetsByHashtagModel,
+    TweetsByListModel: tweetsByListModel,
     mongoose: mongoose
 };
