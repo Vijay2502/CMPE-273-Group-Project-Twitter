9a9cdfc6bf1f588d76242b0dde0b9d5e0c688ddc
ahuja101992 <ahuja101992@gmail.com>
2019-11-12 15:26:29 -0800

    Adding main.js for routing

---
+++ client/src/components/Main.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+
+class Main extends Component {
+  render() {
+    return <div>test</div>;
+  }
+}
+//Export The Main Component
+export default Main;
