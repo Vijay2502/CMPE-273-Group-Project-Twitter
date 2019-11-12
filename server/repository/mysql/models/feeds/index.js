6cbee06911975926092fc382741895c154ac9249
Ayaz Khan <khn.ayzahmd@gmail.com>
2019-11-12 03:15:23 -0800

    UPDATE: defined feeds schema

---
+++ server/repository/mysql/models/feeds/index.js
@@ -0,0 +1,14 @@
+cmodule.exports = function (sequelize, DataTypes) {
+	return sequelize.define('feeds', {
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		data: {
+	        type: DataTypes.JSON,
+	        allowNull: true
+	    }
+	});
+};
