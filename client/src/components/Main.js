c2e4350af609657cf10cab04f09695df78cc3674
ahuja101992 <ahuja101992@gmail.com>
2019-11-14 02:02:02 -0800

    adding sidebar

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import Sidebar from "./Sidebar/sidebar";
 
 class Main extends Component {
   render() {
-    return <div>test</div>;
+    return <div>{<Route path="/" component={Sidebar} />}</div>;
   }
 }
 //Export The Main Component
