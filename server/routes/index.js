a977f094fb8c6b9796cbd41d14b050a63272e277
Ayaz Khan <khn.ayzahmd@gmail.com>
2019-11-18 00:50:47 -0800

    UPDATE: added User apis

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,31 +2,38 @@ const express = require('express');
 const router = express.Router();
 const Tweet = require('../routes/tweets');
 const BookMarkedTweet = require('../routes/bookmarked-tweets');
-const mongoConnection = require("../repository/mongo")
-// const User = require('../user');
+const User = require('./users');
+var passport = require('passport');
+var auth = {
+    userAuth: passport.authenticate('jwt', {session: false})
+};
 
-// var passport = require('passport');
-// var auth = {
-//     userAuth: passport.authenticate('jwt', {session: false})
-// };
-// router.use(passport.initialize());
+router.use(passport.initialize());
 
-// require('../auth/passport')(passport);
-// router.post('/img-upload', auth.userAuth, utils.uploadImage);
+//USER APIS
+router.post('/user/register', User.register);
+router.post('/user/login', User.login);
+router.get('/user/get/:id', User.get);
+router.put('/user/:id/follow', User.follow);
+router.put('/user/:id/unfollow', User.unfollow);
+router.get('/user/:id/followers', User.getFollowers);
+router.get('/user/:id/followees', User.getFollowees);
+router.get('/user/:id/lists/subscriber', User.getListsAsSubscriber);
+router.get('/user/:id/lists/memeber', User.getListsAsMember);
+router.get('/user/:id/lists/owner', User.getListsAsOwner);
 
+
+// TWEET APIS
 router.post('/tweet/create', Tweet.createTweet);
 router.get('/tweet/byOwner/:ownerId', Tweet.getTweetsByOwnerId);
 router.get('/tweet/byId/:tweetId', Tweet.getTweetByTweetId);
 router.put('/tweet/like', Tweet.likeTweet);
 router.put('/tweet/:tweetId/view', Tweet.viewTweet);
-
 router.put('/tweet/bookmark', BookMarkedTweet.bookmarkTweet);
-
 router.post('/tweet/retweet', Tweet.retweet);
 //router.post('/tweet/reply', Tweet.reply);
 router.get('/tweet/bySubscriber', Tweet.getTweetsBySubscriber);
 router.delete('/tweet/:tweetId/delete', Tweet.deleteTweet);
-
 router.get('/tweet/byList/:listId', Tweet.getTweetsByList);
 router.get('/tweet/getByHashtag/:hashtag', Tweet.getByHashtag);
 
