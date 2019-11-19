9c47301dbbebc486d26c10f10c0273c3b0064a6b
Ayaz Khan <khn.ayzahmd@gmail.com>
2019-11-19 01:52:22 -0800

    UPDATE: added list routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const Tweet = require('../routes/tweets');
 const BookMarkedTweet = require('../routes/bookmarked-tweets');
 const User = require('./users');
 const User_producer = require('../kafka-producer').User;
+const List = require('./lists');
 var passport = require('passport');
 var auth = {
     userAuth: passport.authenticate('jwt', {session: false})
@@ -14,15 +15,24 @@ router.use(passport.initialize());
 //USER APIS
 router.post('/user/register', User.register);
 router.post('/user/login', User.login);
-router.get('/user/get/:id', User_producer.get);
+router.get('/user/:id', User_producer.get);
 router.put('/user/:id/follow', User.follow);
 router.put('/user/:id/unfollow', User.unfollow);
 router.get('/user/:id/followers', User.getFollowers);
 router.get('/user/:id/followees', User.getFollowees);
-router.get('/user/:id/lists/subscriber', User.getListsAsSubscriber);
-router.get('/user/:id/lists/memeber', User.getListsAsMember);
-router.get('/user/:id/lists/owner', User.getListsAsOwner);
+router.get('/user/:id/subscriber/lists', User.getListsAsSubscriber);
+router.get('/user/:id/memeber/lists', User.getListsAsMember);
+router.get('/user/:id/owner/lists', User.getListsAsOwner);
 
+//LIST APIS
+router.post('/list/create', List.create);
+router.get('/list/get/:id', List.get);
+router.put('/list/:id/subscribe', List.subscribe);
+router.put('/list/:id/unsubscribe', List.unsubscribe);
+router.get('/list/:id/subscribers', List.getSubscribers);
+router.put('/list/:id/add-member', List.addMember);
+router.put('/list/:id/remove-member', List.removeMember);
+router.get('/list/:id/members', List.getMembers);
 
 // TWEET APIS
 router.post('/tweet/create', Tweet.createTweet);
