c1622f4f189cc629627daa4c697742cf5e0a6908
Ayaz Khan <khn.ayzahmd@gmail.com>
2019-11-30 18:29:12 -0800

    UPDATE: added search page

---
+++ client/src/components/Search/userRow.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import '../../css/hometweetlist.css';
+const UserBox = (props) => {
+    return (
+        <div className="tweet-body">
+            {props.children}
+        </div>
+    )
+};
+
+const Image = (props) => {
+    return (
+        <img src={props.image} alt="Logo" className="picture">
+        </img>
+    )
+};
+
+const Handle = (props) => {
+    return (
+        <div className="handle">
+            {props.handle}
+        </div>
+    )
+};
+
+const Name = (props) => {
+    return (
+        <div className="name">
+            {props.name}
+        </div>
+    )
+};
+
+
+const UserRow = (props) => {
+    return (
+        <div class="list-group">
+            <UserBox>
+                <button type="button" className="inner-body list-group-item list-group-item-action">
+                    <Image image={props.user.data.image} />
+                    <div className="body">
+                        <div className="inner-body-inner">
+                            <Name name={props.user.firstName + " " + props.user.lastName} />
+                            <Handle handle={`@${props.user.username}`} />
+                        </div>                        
+                    </div>
+                </button>
+            </UserBox>
+        </div>
+    )
+};
+
+export { UserRow }
