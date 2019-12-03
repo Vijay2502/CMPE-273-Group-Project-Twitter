fd0ccaa012172e0d45454f3c4c4d822ab2ae4edd
ahuja101992 <ahuja101992@gmail.com>
2019-12-03 05:25:00 -0800

    user profile page for search

--- client/src/components/Profile/userprofile.js
+++ client/src/components/Profile/userprofile.js
@@ -8,7 +8,26 @@ import ViewTweets from "../Tweet/ViewTweets";
 import { getProfile, getfollowees, getfollowers } from "../../redux/actions/userActions";
 import { getTweetsById } from "../../redux/actions/tweetsActions";
 import axios from 'axios';
-
+import { Link } from "react-router-dom";
+import UserList from '../Search/User/UserList';
+import InfiniteScroll from 'react-infinite-scroller';
+import FollowList from "./userlist";
+function mapStateToProps(store) {
+    return {
+        userDetails: store.users.userDetails,
+        userTweets: store.tweets.userTweets,
+        userFollowers: store.users.follower,
+        userFollowee: store.users.followee
+    };
+}
+function mapDispatchToProps(dispatch) {
+    return {
+        getProfileDetails: data => dispatch(getProfile(data)),
+        getUserTweets: data => dispatch(getTweetsById(data)),
+        getUserfollowers: data => dispatch(getfollowers(data)),
+        getUserfollowees: data => dispatch(getfollowees(data))
+    }
+}
 
 class userprofile extends Component {
     constructor(props) {
@@ -17,82 +36,197 @@ class userprofile extends Component {
             editProfile: false, //for modal
             selectedCoverPic: null,
             selectedProfilePic: null,
-            users: []
+            openFollower: false,
+            users: [],
+            openFollowee: false
         };
-        // this.onCoverPicUpload = this.onCoverPicUpload.bind(this);
-        // this.onProfilePicUpload = this.onProfilePicUpload.bind(this);
     }
     componentWillMount = () => {
-        // const data = {
-        //     user_id: localStorage.getItem('id')
-        // };
+        const data = {
+            user_id: this.props.user_id
+        };
 
-        // this.props.getProfileDetails(data);
-        // this.props.getUserTweets({ user_id: 1 });
-        // this.props.getUserfollowees(data);// ISSUE WITH API SO COMMENTING
-        // this.props.getUserfollowers(data);// ISSUE WITH API SO COMMENTING
+        this.props.getProfileDetails(data);
+        this.props.getUserTweets(data);
+        this.props.getUserfollowees(data);// ISSUE WITH API SO COMMENTING
+        this.props.getUserfollowers(data);// ISSUE WITH API SO COMMENTING
         // this.getUser();
     }
 
+    followerList = () => {
+        this.setState({ openFollower: true });
+    }
+    closefollowerList = () => {
+        this.setState({ openFollower: false });
+    }
+    followeeList = () => {
+        this.setState({ openFollowee: true });
+    }
+    closefolloweeList = () => {
+        this.setState({ openFollowee: false });
+    }
     componentDidMount() {
+    }
+    getDate = () => {
+        if (this.props.userDetails) {
+            if (this.props.userDetails.createdAt) {
+                let date = this.props.userDetails.createdAt;
+                console.log("kkkk:", date);
+                let newDate = new Date(date);
+                let day = newDate.getDay() + 1
+                var month = new Array();
+                month[0] = "Jan";
+                month[1] = "Feb";
+                month[2] = "Mar";
+                month[3] = "Apr";
+                month[4] = "May";
+                month[5] = "Jun";
+                month[6] = "Jul";
+                month[7] = "Aug";
+                month[8] = "Sep";
+                month[9] = "Oct";
+                month[10] = "Nov";
+                month[11] = "Dec";
+                var mon = month[newDate.getMonth()];
+                let joinedDate = day + "-" + mon + "-" + newDate.getFullYear();
+                console.log(" date :", joinedDate);
+                return joinedDate;
+            } else {
+                return ""
+            }
+
+        } else {
+            return " "
+        }
+    }
+    unFollowUser = () => {
+        axios.put('http://localhost:8080/api/v1/user/' + this.props.user_id + '/unfollow')
+            .then(res => {
+                console.log("test result :", res);
+                if (res.data.status === "ok") {
+                    alert("Profile unfollowed successfully");
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+    followUser = () => {
+        axios.put('http://localhost:8080/api/v1/user/' + this.props.user_id + '/follow')
+            .then(res => {
+                console.log("test result :", res);
+                if (res.data.status === "ok") {
+                    alert("Profile followed successfully");
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+    followUnfollowBtn = () => {
+        if (this.props.userDetails) {
+            if (this.props.userDetails.followed) {
+                return (<button
+                    type="button"
+                    // style="display: none"
+                    onClick={this.unFollowUser}
+                    class="btn btn-primary edit-profile-btn"
+                >
+                    Unfollow
+                </button>)
+            }
+            else {
+                return (<button
+                    type="button"
+                    // style="display: none"
+                    onClick={this.followUser}
+                    class="btn btn-primary edit-profile-btn"
+                >
+                    Follow
+                </button>)
+            }
+        } else {
+            return "";
+        }
     }
     render() {
         console.log("checking props", JSON.stringify(this.props));
+        let usrDetails = this.props.userDetails ? this.props.userDetails : [];
+        let usrTweets = this.props.userTweets ? this.props.userTweets : [];
+        let tweetCount = usrTweets.length ? usrTweets.length : 0;
+        let usrFollower = this.props.userFollowers ? this.props.userFollowers : [];
+        let usrFollowee = this.props.userFollowee ? this.props.userFollowee : [];
+        let userData = usrDetails.data ? usrDetails.data : [];
+        // console.log("usrFollowee length :", Object.keys(usrFollowee).length);
+        if (this.state.openFollower) {
+            return (<FollowList openFollower={this.state.openFollower} title={"Follower List"} closeList={this.closefollowerList}
+                users={usrFollower.followers}
+                profile={usrDetails}
+                getUsers={usrDetails}
+                hasMore={false}
+            ></FollowList>)
+        }
+        else if (this.state.openFollowee) {
+            return (<FollowList openFollowee={this.state.openFollowee} title={"Followee List"} closeList={this.closefolloweeList}
+                users={usrFollowee.followees}
+                profile={usrDetails}
+                getUsers={usrDetails}
+                hasMore={false}
+            ></FollowList>)
+        }
+
         return (
             <div class="profile-container col-sm-12">
                 <div class="top-details row">
                     <div class="offset-sm-1">
-                        <div class="profile-name-header">{this.props.firstName ? this.props.firstName : "Test"} {this.props.lastName ? this.props.lastName : "Test"}</div>
-                        {/* <div class="profile-tweets-header">{tweetCount} tweets</div> */}
+                        <div class="profile-name-header">{usrDetails.firstName ? usrDetails.firstName : "" + " " + usrDetails.lastName ? usrDetails.lastName : ""}</div>
+                        <div class="profile-tweets-header">{tweetCount} tweets</div>
                     </div>
                 </div>
                 <div class="profile-cover-pic row">
                     <img
-                        src={this.props.coverPic ? this.props.coverPic : require("../../static/images/cover_pic1.png")}
+                        src={userData.coverPic ? userData.coverPic : require("../../static/images/cover_pic1.png")}
                         width="100%"
                         height="200px"
                     />
                 </div>
                 <div class="profile-pic-btn-container row">
                     <div class="profile-profile-pic col-sm-6">
-                        <img src={this.props.profileImage ? this.props.profileImage : require("../../static/images/profile_pic.png")} height="120" width="120px" />
+                        <img src={userData.profileImage ? userData.profileImage : require("../../static/images/profile_pic.png")} height="120" width="120px" />
                     </div>
                     <div class="col-sm-6 edit-btn">
-                        <button
-                            type="button"
-                            onClick={this.editProfile}
-                            class="btn btn-primary edit-profile-btn"
-                        >
-                            Change according to props.......
-                        </button>
+                        {this.followUnfollowBtn}
                     </div>
                 </div>
                 <div class="profile-details row">
                     <div class="col-sm-12">
-                        <div class="profile-name-header ">{this.props.firstName ? this.props.firstName : "test"} {this.props.lastName ? this.props.lastName : "Test"}</div>
-                        <div class="profile-detail-font">@{this.props.username ? this.props.username : "test"}</div>
+                        <div class="profile-name-header ">{usrDetails.firstName ? usrDetails.firstName : ""} {usrDetails.lastName ? usrDetails.lastName : ""}</div>
+                        <div class="profile-detail-font">@{usrDetails.username ? usrDetails.username : ""}</div>
                         <div class="profile-dates row">
-                            <div class="col-sm-4 profile-detail-font">
+                            {/* <div class="col-sm-4 profile-detail-font">
                                 <FontAwesomeIcon icon={faBirthdayCake} />
                                 <span> born {}</span>
-                            </div>
-                            <div class="col-sm-4 profile-detail-font">
+                            </div> */}
+                            <div class="col-sm-8 profile-detail-font">
                                 <FontAwesomeIcon icon={faCalendarAlt} />
-                                <span> Joined {}</span>
+                                <span> Joined {this.getDate()}</span>
                             </div>
                         </div>
                         <div class="followers-following row">
-                            <div class="col-sm-3 profile-detail-font">{this.props.count ? this.props.count : 0} Followers</div>
-                            <div class="offset-sm-1 col-sm-3 profile-detail-font">{this.props.count ? this.props.count : 0} Following</div>
+                            <div class="col-sm-3 profile-detail-font"><Link class="link-color" onClick={this.followerList}>{usrFollower.count ? usrFollower.count : 0} Followers</Link></div>
+                            <div class="offset-sm-1 col-sm-3 profile-detail-font"><Link class="link-color" onClick={this.followeeList}>{usrFollowee.count ? usrFollowee.count : 0} Following</Link></div>
+                        </div>
+                        <div class="followers-following row">
+                            <div class="col-sm-10 profile-detail-font">{userData.state ? userData.state : ""} {userData.city ? userData.city : ""} {userData.zipcode ? userData.zipcode : ""}</div>
                         </div>
                     </div>
                 </div>
                 <div class="heading row"><div class="tweets-heading col-sm-4">Tweets</div></div>
                 <div class="tweets-list" row>
-                    <ViewTweets dataFromParent={this.props.usrTweets ? this.props.usrTweets : []} />
+                    <ViewTweets dataFromParent={usrTweets} />
                 </div>
             </div>
         );
     }
 }
-export default userprofile;
+export default connect(mapStateToProps, mapDispatchToProps)(userprofile);
